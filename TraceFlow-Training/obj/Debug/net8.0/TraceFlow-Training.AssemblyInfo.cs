620d00250222123dece79f73709d89c26b1fa414
gugomesx10 <gustavogomesmartins4@icloud.com>
2026-05-13 10:47:42 -0400

    chore: configure gitignore

--- TraceFlow-Training/obj/Debug/net8.0/TraceFlow-Training.AssemblyInfo.cs
+++ TraceFlow-Training/obj/Debug/net8.0/TraceFlow-Training.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TraceFlow-Training")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8de332a883b2fc7c26389878bc827f3395a0de72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7e0b47c8f2c9142fbbd169d56b2458ea5e91f4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TraceFlow-Training")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TraceFlow-Training")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TraceFlowTraining.Infrastructure/obj/Debug/net8.0/TraceFlowTraining.Infrastructure.AssemblyInfo.cs
+++ TraceFlowTraining.Infrastructure/obj/Debug/net8.0/TraceFlowTraining.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TraceFlowTraining.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7e0b47c8f2c9142fbbd169d56b2458ea5e91f4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TraceFlowTraining.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TraceFlowTraining.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
