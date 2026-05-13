d7e0b47c8f2c9142fbbd169d56b2458ea5e91f4d
gugomesx10 <gustavogomesmartins4@icloud.com>
2026-05-13 10:28:55 -0400

    feat: add initial domain entities and enum

---
+++ TraceFlowTraining.Domain/Entities/Warehouse.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TraceFlowTraining.Domain.Entities;
+
+[Table("TB_WAREHOUSE")]
+public class Warehouse
+{
+    [Key]
+    [Column("ID")]
+    public Guid Id { get; set; }
+    
+    [Required]
+    [Column("NAME")]
+    [StringLength(100)]
+    public string Name { get; set; } =  String.Empty;
+    
+    [Required]
+    [Column("LOCATION")]
+    [StringLength(200)]
+    public string Location { get; set; } =  String.Empty;
+    
+    [Required]
+    [Column("CAPACITY")]
+    [Range(1, int.MaxValue)]
+    public int Capacity { get; set; } 
+}
